823f292909d87b17e62e24ae705dbdff0f2c4f0f
nikhilniwateArhamLabs <nikhilniwate@arhamlabs.com>
2022-12-12 12:14:36 +0530

    Commented to see if it removes lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,15 +1,15 @@
-module.exports = {
-  ci: {
-    upload: {
-      target: "temporary-public-storage",
-    },
-    assert: {
-      preset: "lighthouse:no-pwa",
-      assertions: {
-        "preload-lcp-image": "off",
-        "unused-javascript": "off",
-        "csp-xss": "off",
-      },
-    },
-  },
-};
+// module.exports = {
+//   ci: {
+//     upload: {
+//       target: "temporary-public-storage",
+//     },
+//     assert: {
+//       preset: "lighthouse:no-pwa",
+//       assertions: {
+//         "preload-lcp-image": "off",
+//         "unused-javascript": "off",
+//         "csp-xss": "off",
+//       },
+//     },
+//   },
+// };
