e00998e34bd8844f0fc2d0d9e3782707d219a910
nikhilniwateArhamLabs <nikhilniwate@arhamlabs.com>
2022-12-26 17:54:37 +0530

    Removed Url

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,8 @@
 module.exports = {
   ci: {
-    collect: {
-      url: ["http://localhost:3000/"],
-    },
+    // collect: {
+    //   url: ["http://localhost:3000/"],
+    // },
     upload: {
       target: "temporary-public-storage",
     },
