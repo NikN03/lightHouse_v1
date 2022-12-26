f5aabb40ab765c951699b46f4a2d0077fcf2aaf9
nikhilniwateArhamLabs <nikhilniwate@arhamlabs.com>
2022-12-26 18:00:36 +0530

    Added static Dist

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,9 @@
 module.exports = {
   ci: {
-    // collect: {
-    //   url: ["http://localhost:3000/"],
-    // },
+    collect: {
+      staticDistDir: "./dist",
+      url: ["http://localhost:3000/"],
+    },
     upload: {
       target: "temporary-public-storage",
     },
